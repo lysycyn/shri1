93e955e921eed02366fcc7c8a00bf186f4537b7f
Danil Lewin <danillewin@gmail.com>
2018-02-03 16:12:32 +0300

    mock data

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const graphqlRoutes = require('./graphql/routes');
 
 const app = express();
 
+app.use('/graphql', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 app.use(bodyParser.json());
 
 app.use('/', pagesRoutes);
