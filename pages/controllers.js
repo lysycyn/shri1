10ff81f1c2ed281b1e9897ed58c3ed82af90d907
Danil Lewin <danillewin@gmail.com>
2018-02-03 17:05:38 +0300

    static distribution

--- pages/controllers.js
+++ pages/controllers.js
@@ -1,11 +1,5 @@
+const path = require('path');
+
 module.exports.index = (req, res) => {
-    res.send(`
-    <html>
-      <head>
-        <title>Shri 2018</title>
-        <script src="/scripts/test.js"></script>
-      </head>
-      <h1>Hello</h1>
-    </html>
-    `);
+    res.sendFile(path.resolve('./build/main.html'));
 };
