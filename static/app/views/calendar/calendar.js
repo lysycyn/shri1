7855f75017a8a5a428a0d789287e27a37d2b9028
Danil Lewin <danillewin@gmail.com>
2018-02-04 17:23:41 +0300

    calendar and tooltips

--- static/app/views/calendar/calendar.js
+++ static/app/views/calendar/calendar.js
@@ -1,6 +1,11 @@
 const View = require('../view');
 const template = require('./calendar.mustache');
 
+const utils = require('../../utils');
+
+const CALENDAR_TIMLINE_MINUTES = 900;
+const ONE_MINUTE_LENGTH_PERCENTS = 100 / CALENDAR_TIMLINE_MINUTES;
+
 /**
  * Class representing a Calendar View
  */
@@ -14,7 +19,46 @@ class CalendarView extends View {
     constructor(container, model) {
         super(template, container, model);
 
-        console.log('MODEL:', model);
+        this._model.rooms = this._model.rooms.map((room) => {
+            const roomExtended = Object.assign(room);
+
+            roomExtended.events = roomExtended.events.map((event) => {
+                const { dateEnd, dateStart, users } = event;
+
+                const durationMinutes = (dateEnd - dateStart) / 1000 / 60;
+                const offsetMinutes = ((dateEnd.getHours() - 8) * 60) + dateEnd.getMinutes();
+
+                const left = (offsetMinutes * ONE_MINUTE_LENGTH_PERCENTS).toFixed(3);
+                const width = (durationMinutes * ONE_MINUTE_LENGTH_PERCENTS).toFixed(3);
+
+                const dateText = utils.getDateText(dateStart);
+                const startTimeText = utils.getTimeText(dateStart);
+                const endTimeText = utils.getTimeText(dateEnd);
+                const participantName = users[0].login;
+
+                const participantsPlural = utils.plural(users.length - 1, [
+                    'участник',
+                    'участника',
+                    'участников',
+                ]);
+
+                const participantsCountText = `и ещё ${users.length - 1} ${participantsPlural}`;
+
+                return {
+                    id: event.id,
+                    title: event.title,
+                    left,
+                    width,
+                    dateText,
+                    startTimeText,
+                    endTimeText,
+                    participantName,
+                    participantsCountText,
+                };
+            });
+
+            return roomExtended;
+        });
     }
 }
 
